90044517bfe482e3c425420f993bb9e1534dbe9a
MapleLeaf2007 <197728340@qq.com>
2025-07-31 17:10:52 +0800

    refactor: 重构周报功能，优化代码结构和逻辑 fix: 修复插件加载失败时的错误提示信息 fix: 修复数据目录创建路径，确保正确性 style: 统一代码风格，调整函数参数格式

--- components/Render.js
+++ components/Render.js
@@ -1,16 +1,16 @@
 import { pluginRoot } from '../model/path.js'
 
 export default {
-  async render (tpl, data = {}, cfg = {}) {
+  async render(tpl, data = {}, cfg = {}) {
     let { e } = cfg
     if (!e) return false
 
     return e.runtime.render('delta-force-plugin', tpl, data, {
       ...cfg,
-      beforeRender ({ data }) {
+      beforeRender({ data }) {
         const _res_path = data.pluResPath
         return {
-      ...data,
+          ...data,
           _res_path: `${_res_path}/`,
           defaultLayout: `${pluginRoot}/resources/common/layout/default.html`
         }
