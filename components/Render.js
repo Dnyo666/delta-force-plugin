6ea4c23fa96156315c7244b7d3325693ca818e57
MapleLeaf2007 <197728340@qq.com>
2025-08-02 12:31:44 +0800

    细节优化

--- components/Render.js
+++ components/Render.js
@@ -1,4 +1,12 @@
 import { pluginRoot } from '../model/path.js'
+import Config from './Config.js'
+
+function scale(pct = 1, customScale = 1) {
+  const renderScale = (Config.getConfig())?.render_scale || 10
+  const baseScale = Math.min(2, Math.max(0.5, renderScale))
+  pct = pct * baseScale * customScale
+  return `style=transform:scale(${pct})`
+}
 
 export default {
   async render(tpl, data = {}, cfg = {}) {
@@ -11,6 +19,9 @@ export default {
         const _res_path = data.pluResPath
         return {
           ...data,
+          sys: {
+            scale: scale()
+          },
           _res_path: `${_res_path}/`,
           defaultLayout: `${pluginRoot}/resources/common/layout/default.html`
         }
